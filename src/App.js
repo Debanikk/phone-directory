575b5b87d3880f4a47c2fa8b2dbfa539c0ab6765
Debanikk <debanikkundu2014@gmail.com>
2021-03-06 08:10:17 +0530

    Styled ‘Header’ (External Styling)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
-
+import Header from "./Header";
 
 class App extends Component {
   render(){
     return (
-      <div className="header">
-        <div>
-          Phone Directory
-        </div>
+      <div>
+        <Header />
         <button> Add </button>
         <div>
           <span> Name </span><br/>
           <span> Phone </span>
         </div>
-        {/* <label htmlFor="name">Name: </label>
-        <input id="name" type="text" placeholder="Type Here" defaultValue="Debanik"/> */}
+        
       </div>
+     
     );
   }
   
