6901a60836c17d553bb856bab446d41d10d04699
Debanikk <debanikkundu2014@gmail.com>
2021-03-06 16:42:27 +0530

    Passed heading to Header component as props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
 import Header from "./Header";
+import './App.css';
 
 class App extends Component {
   render(){
+    let subscribers = [
+      {
+        id: 1,
+        name: "Debanik Kundu",
+        phone: "9158314321"
+      },
+      {
+        id: 2,
+        name: "Payal Nath",
+        phone: "9168667520"
+      },
+      {
+        id: 3,
+        name: "Hiranmayee Kundu",
+        phone: "0123456789"
+      }
+    ]
     return (
       <div>
-        <Header />
-        <button> Add </button>
-        <div>
-          <span> Name </span><br/>
-          <span> Phone </span>
+        <Header heading="Phone Directory" instructor="Debanik Kundu"/>
+        <div className="component-body-container">
+          <button className="custom-btn add-btn">Add</button>
+
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+          </div>
         </div>
+        
+        {
+          subscribers.map(sub => {
+            return <div key={sub.id} className="grid-container">
+              <span className="grid-item">{sub.name}</span>
+              <span className="grid-item">{sub.phone}</span>
+              <button className="custom-btn dlt-btn">Delete</button>
+              </div>
+          })
+        }
         
       </div>
      
@@ -20,3 +51,4 @@ class App extends Component {
 }
 
 export default App;
+
